ac223397a95031d2296b49912639b895de0958ab
deborah reis <deborahmreis3@gmail.com>
2022-05-25 15:12:23 +0100

    mais um pouco

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+
+def prepare
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_name
+  phone_number = Faker::Number.number(digits: 10)
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  {name: name, address: address, phone_number: phone_number, category: category}
+end
+
+5.times do
+  Restaurant.create(prepare)
+end
